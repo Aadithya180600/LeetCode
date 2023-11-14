54a0408311fa00c6ae208dee32beb4d2ba247eaa
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2023-11-14 15:39:24 -0600

    Time: 44 ms (51.28%), Space: 12.6 MB (63.04%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator()(int a, int b) const { return a < b; }
+    };
+    string sortVowels(string s) {
+        map<char, int, cmp> m;
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            if(s[i]=='A' || s[i] == 'E' || s[i]=='I' || s[i] == 'O' || s[i] == 'U'){
+                m[s[i]]++;
+            }
+            else if(s[i]=='a' || s[i] == 'e' || s[i]=='i' || s[i] == 'o' || s[i] == 'u'){
+                m[s[i]]++;
+            }
+        }
+        auto it = m.begin();
+        string t = "";
+        for(int i = 0;i<n;i++){
+            if(m.find(s[i]) == m.end()) t+=s[i];
+            else{
+                t+=it->first;
+                it->second--;
+                if(it->second == 0) it++;
+            }
+        }
+        return t;
+    }
+};
