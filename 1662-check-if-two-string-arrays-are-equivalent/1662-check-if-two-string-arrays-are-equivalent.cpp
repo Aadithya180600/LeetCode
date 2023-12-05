474f2b7db17da1fad4a4a0bf2e5861b996b19f2a
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2023-12-04 18:32:13 -0600

    Time: 5 ms (25.11%), Space: 12 MB (44.43%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        int i = 0, j = 0;
-        int n = 0, m = 0;
-        while(i<word1.size() & j<word2.size()){
-            if(word1[i][n++] != word2[j][m++]){
-                return false;
-            }
-            if(n == word1[i].size()) {
-                i++;
-                n = 0;
-            }
+        
+        string s = "", t = "";
+        for(string i:word1){
+            s+=i;
+        }
+        for(string j:word2){
+            t+=j;
+        }
+        return s == t;
+        // int i = 0, j = 0;
+        // int n = 0, m = 0;
+        // while(i<word1.size() & j<word2.size()){
+        //     if(word1[i][n++] != word2[j][m++]){
+        //         return false;
+        //     }
+        //     if(n == word1[i].size()) {
+        //         i++;
+        //         n = 0;
+        //     }
             
-            if(m == word2[j].size()){
-                j++;
-                m = 0;
-            }
-        }
-        if(i<word1.size() or j<word2.size()) {
-            return false;
-        }
-        return true;
+        //     if(m == word2[j].size()){
+        //         j++;
+        //         m = 0;
+        //     }
+        // }
+        // if(i<word1.size() or j<word2.size()) {
+        //     return false;
+        // }
+        // return true;
     }
 };
