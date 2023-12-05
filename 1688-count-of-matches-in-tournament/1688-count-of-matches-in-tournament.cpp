d309542d06d09a83b182e0f4e2978215d4407d56
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2023-12-04 18:21:38 -0600

    Time: 0 ms (100.00%), Space: 6.3 MB (73.09%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int c = 0;
+        int r = 0;
         while(n!=1){
             if(n%2==0){
                 n/=2;
-                c+=n;
+                r+=n;
             }
             else{
                 n = ((n-1)/2);
-                c+=n;
+                r+=n;
                 n++;
             }
         }
-        return c;
+        return r;
     }
 };
