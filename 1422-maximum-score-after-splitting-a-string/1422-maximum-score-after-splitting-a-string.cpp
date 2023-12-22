05aab2ef6241284c490024068db3c2d7db420f6e
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2023-12-22 17:50:38 -0600

    Time: 3 ms (50.00%), Space: 6.8 MB (43.36%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int maxScore(string s) {
         int n = s.length();
-        int m = 0, zz=0, oo=0;
-        vector<int> z(n), o(n);
+        int m = 0, o = 0;
         for(int i = 0;i<n;i++){
-            if(s[i] == '0') {
-                zz++;
-                z[i] = zz;
-            }
-            if(s[n-i-1] == '1') {
-                oo++;
-                o[n-i-1] = oo;
-            }
-            // cout<<i<<"\n";
+            if(s[i] == '1') o++;
         }
+        
+        int nz = 0;
         for(int i = 0;i<n-1;i++){
-            cout<<z[i]<<" "<<o[i+1]<<"\n";
-            if(m < z[i]+o[i+1]) m = z[i]+o[i+1];
+            if(s[i] == '0'){
+                nz++;
+            }
+            else{
+                o--;
+            }
+            if(m < o+nz){
+                m = o+nz;
+            }
         }
         return m;
     }
