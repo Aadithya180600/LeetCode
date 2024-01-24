bf978595f4950c5eae5b8bde9bc30a4390409f6c
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2024-01-24 09:30:52 +0530

    Time: 6 ms (92.66%), Space: 13.8 MB (5.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -8,13 +8,14 @@ static const int fast_io = []()
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> m;
-        int n = (int) nums.size();
-        for(int i=0;i<n;i++){
-            if(m.find(target-nums[i])!=m.end()) {
-                return {i, m[target-nums[i]]};
+        unordered_map<int, int> m;
+        for(int i=0;i<nums.size();i++){
+            if(m.find(nums[i]) != m.end()){
+                return {i, m[nums[i]]};
             }
-            else m[nums[i]] = i;
+            else{
+                m[target - nums[i]] = i;
+            }
         }
         return {};
     }
