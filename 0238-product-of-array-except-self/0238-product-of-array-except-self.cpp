ac5f58098e79221d4db3145dc344a3971678f381
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2024-01-26 08:20:07 +0530

    Time: 18 ms (65.32%), Space: 26.5 MB (9.38%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        int l[n];
-        int r[n];
-        l[0] = nums[0];
+        int l = 1;
+        vector<int> r(n);
+        // l[0] = nums[0];
         r[n-1] = nums[n-1];
-        for(int i = 1; i<nums.size();i++){
-            l[i] = nums[i]* l[i-1];
-        }
+        // for(int i = 1; i<nums.size();i++){
+        //     l[i] = nums[i]* l[i-1];
+        // }
         for(int i = n-2;i>=0;i--){
             r[i] = r[i+1]*nums[i];
         }
-        vector<int> ans(n);
+
         for(int i = 0;i<n;i++){
             if(i == 0){
-                ans[i] = r[i+1];
+                r[i] = r[i+1];
+                l = l*nums[i];
             }
             else if(i == n-1)
             {
-                ans[i] = l[i-1];
+                r[i] = l;
             }
             else{
-                ans[i] = l[i-1]*r[i+1];
+                r[i] = l*r[i+1];
+                l = l * nums[i];
             }
         }
-        return ans;
+        return r;
     }
 };
