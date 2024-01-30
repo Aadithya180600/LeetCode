64b5e20654eaa951bd4a5cf2c67ca8c56841f569
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2024-01-30 16:12:50 -0600

    Time: 63 ms (87.79%), Space: 16.8 MB (87.26%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,40 +2,13 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        encoded_strs = [i.encode().hex() for i in strs]
-        # print(encoded_strs)
-        # return " ".join(encoded_strs)
-        s = ""
-        for i in strs:
-            for j in i:
-                s+=(str(ord(j)))
-                s+=" "
-            s+="/"
-        print(s)
-        return s
+        return '\U0001f602'.join(strs)
+        
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-         # decoded_strs = [i.decode().hex() for i in strs]
-         # print(encoded_strs)
-        l = []
-        r = ""
-        k = ""
-        for i in s:
-            if i==" ":
-                r+=chr(int(k))
-                k=""
-            elif i == "/":
-                l.append(r)
-                r =""
-                k = ""
-            else:
-                k+=i
-        return l
-                
-        
-        
+        return s.split('\U0001f602')
 
 
 # Your Codec object will be instantiated and called as such:
