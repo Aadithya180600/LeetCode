8f9397dd0d6f295f7773f4b68368fa2c5b1640d3
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2024-07-30 17:26:19 -0500

    Time: 489 ms (98.20%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -6,4 +6,4 @@ SUM(IF(state = 'approved', 1, 0)) AS approved_count,
 SUM(amount) AS trans_total_amount,
 SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
 FROM Transactions
-GROUP BY country, month;
+GROUP BY country, DATE_FORMAT(trans_date, "%Y-%m");
