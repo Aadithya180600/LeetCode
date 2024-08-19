2d0bdb0800a9138c5abdfcbcaabd556c3305825d
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2024-08-19 17:09:55 -0500

    Time: 3 ms (69.44%), Space: 7.3 MB (77.36%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -4,7 +4,7 @@ public:
         if(n == 1) return 0;
         for(int i = 2;i<=n;i++){
             if(n%i == 0){
-                return i+ fun(n/i);
+                return fun(n/i) + i;
             }
         }
         return 0;
