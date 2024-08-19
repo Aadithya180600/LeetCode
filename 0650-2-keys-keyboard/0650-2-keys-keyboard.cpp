693496268502fb1cd6127235f91b125484c48e41
Sai Sangameswara Aadithya Kanduri <68139590+Aadithya180600@users.noreply.github.com>
2024-08-19 17:10:26 -0500

    Time: 0 ms (100.00%), Space: 7.6 MB (56.27%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int fun(int n){
-        if(n == 1) return 0;
-        for(int i = 2;i<=n;i++){
-            if(n%i == 0){
-                return fun(n/i) + i;
+        if(n !=1){
+            for(int i = 2;i<=n;i++){
+                if(n%i == 0){
+                    return fun(n/i) + i;
+                }
             }
         }
         return 0;
